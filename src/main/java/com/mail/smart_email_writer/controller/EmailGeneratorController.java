21d800aa6eb9ecbf95a7ef327904a09871c4da9f
Asiman <asiman.ismayilova.2006@mail.ru>
2025-01-24 11:09:39 +0400

    update controller to handle email generation requests

--- src/main/java/com/mail/smart_email_writer/controller/EmailGeneratorController.java
+++ src/main/java/com/mail/smart_email_writer/controller/EmailGeneratorController.java
@@ -1,19 +1,28 @@
 package com.mail.smart_email_writer.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mail.smart_email_writer.entity.EmailRequest;
+import com.mail.smart_email_writer.service.EmailGeneratorService;
 
 @RestController
 @RequestMapping("/api/email")
 public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    public EmailGeneratorController(EmailGeneratorService emailGeneratorService) {
+        this.emailGeneratorService = emailGeneratorService;
+    }
     
+    @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
-        //
-        return ResponseEntity.ok(""); // TODO: returns generated email
+        String response = emailGeneratorService.generateReplyEmail(emailRequest);
+        return ResponseEntity.ok(response); 
         
     }
 }
