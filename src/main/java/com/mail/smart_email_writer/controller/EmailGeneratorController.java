5f1d31d631e005e49c35bcc480f65ccc5c503601
Asiman <asiman.ismayilova.2006@mail.ru>
2025-01-25 13:00:18 +0400

    finish with backend logic-API related error resolved

--- src/main/java/com/mail/smart_email_writer/controller/EmailGeneratorController.java
+++ src/main/java/com/mail/smart_email_writer/controller/EmailGeneratorController.java
@@ -6,14 +6,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.mail.smart_email_writer.entity.EmailRequest;
 import com.mail.smart_email_writer.service.EmailGeneratorService;
 
 @RestController
 @RequestMapping("/api/email")
 @CrossOrigin(origins = "*") // Allow requests from any domain
-
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
